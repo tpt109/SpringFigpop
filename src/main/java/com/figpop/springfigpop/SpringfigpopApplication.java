630b9ab5f5dc4dbd51bbefcd3e1766ed26641706
toan tran <tpt109@gmail.com>
2021-11-03 23:38:10 +0700

    init

---
+++ src/main/java/com/figpop/springfigpop/SpringfigpopApplication.java
@@ -0,0 +1,13 @@
+package com.figpop.springfigpop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfigpopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringfigpopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/figpop/springfigpop/SpringfigpopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.figpop.springfigpop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfigpopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
